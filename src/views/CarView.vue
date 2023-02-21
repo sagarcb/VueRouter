ef485efcbcf3de750f42391a9cdd7bb8156721d3
sagar <sagar@bitmascot.com>
2023-02-21 18:33:01 +0600

    conditional routing and child routing

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -1,10 +1,17 @@
 <script setup>
-  import {useRoute} from "vue-router"
+import {useRoute, useRouter, RouterView} from "vue-router"
   import cars from "../data/cars.json"
 
   const route = useRoute();
+  const router = useRouter();
 
-  const car = cars.find(c => c.id === parseInt(route.params.id));
+const carId = parseInt(route.params.id);
+const car = cars.find(c => c.id === carId);
+
+const showContact = () => {
+  if (carId === 4) return;
+  return router.push(`/cars/${carId}/contact`);
+}
 </script>
 <template>
   <div>
@@ -12,6 +19,7 @@
     <p>Name: {{car.name}}</p>
     <p>Year: {{car.year}}</p>
     <p>Price: {{car.price}}</p>
+    <button @click="showContact">Click for contact</button>
     <RouterView/>
   </div>
 </template>
