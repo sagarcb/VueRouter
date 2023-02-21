af277e01bcdba9732d4c9e6a4f2d29be0862062e
sagar <sagar@bitmascot.com>
2023-02-21 18:38:21 +0600

    if else condition added in render view

--- src/views/CarView.vue
+++ src/views/CarView.vue
@@ -1,25 +1,28 @@
 <script setup>
-import {useRoute, useRouter, RouterView} from "vue-router"
+  import {useRoute, useRouter, RouterView} from "vue-router"
   import cars from "../data/cars.json"
 
   const route = useRoute();
   const router = useRouter();
 
-const carId = parseInt(route.params.id);
-const car = cars.find(c => c.id === carId);
+  const carId = parseInt(route.params.id);
+  const car = cars.find(c => c.id === carId);
 
-const showContact = () => {
-  if (carId === 4) return;
-  return router.push(`/cars/${carId}/contact`);
-}
+  const showContact = () => {
+    if (carId === 4) return;
+    return router.push(`/cars/${carId}/contact`);
+  }
 </script>
 <template>
-  <div>
+  <div v-if="car">
     <h1>Car View</h1>
     <p>Name: {{car.name}}</p>
     <p>Year: {{car.year}}</p>
     <p>Price: {{car.price}}</p>
     <button @click="showContact">Click for contact</button>
     <RouterView/>
+  </div>
+  <div v-else>
+    <h1>Car not found</h1>
   </div>
 </template>
