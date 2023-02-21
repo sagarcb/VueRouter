c605f1cef04290672ee4278defff08a94ae279b8
sagar <sagar@bitmascot.com>
2023-02-21 17:55:11 +0600

    nested route and dynamic route

---
+++ src/views/CarView.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import {useRoute} from "vue-router"
+  import cars from "../data/cars.json"
+
+  const route = useRoute();
+
+  const car = cars.find(c => c.id === parseInt(route.params.id));
+</script>
+<template>
+  <div>
+    <h1>Car View</h1>
+    <p>Name: {{car.name}}</p>
+    <p>Year: {{car.year}}</p>
+    <p>Price: {{car.price}}</p>
+    <RouterView/>
+  </div>
+</template>
